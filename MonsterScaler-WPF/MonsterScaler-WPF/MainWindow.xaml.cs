86995676648b9379751b15ca55dae6aa40953c8a
Musaab Yousif <musaab.o.yousif@wmich.edu>
2018-05-16 16:27:36 -0400

    Started WPF project

---
+++ MonsterScaler-WPF/MonsterScaler-WPF/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace MonsterScaler_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.ShowDialog();
+            dialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            string filename = dialog.FileName;
+            fileNameTextBox.Text = filename;
+
+            JsonRead(filename);
+        }
+    }
+}
---
+++ MonsterScaler-WPF/MonsterScaler-WPF/Monster.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterScaler_WPF
+{
+    class Monster
+    {
+        //Source page: https://raw.githubusercontent.com/BTMorton/dnd-5e-srd/master/json/11%20monsters.json
+        
+        string name;
+        int armorClass;
+        int hitPoints;
+        int hitPointDice;
+        int speed;
+
+        int strScore;
+        string strMod;
+        int dexScore;
+        string dexMod;
+        int conScore;
+        string conMod;
+        int intScore;
+        string intMod;
+        int wisScore;
+        string wisMod;
+        int chaScore;
+        string chaMod;
+
+        string strSavingThrowMod;
+        string dexSavingThrowMod;
+        string conSavingThrowMod;
+        string intSavingThrowMod;
+        string wisSavingThrowMod;
+        string chaSavingThrowMod;
+
+        string d
+
+    }
+}
